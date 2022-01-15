79e31c975469f5d38e04a7cbec17ddb2056bf8cd
Rami <44623786+Rami-J@users.noreply.github.com>
2022-01-15 13:42:07 -0500

    console log tests

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 
 exports.handler = async (event) => {
     console.log(event);
+    console.log(event.userIdentity);
+    console.log(event.requestParameters);
+    console.log(event.responseElements);
+    console.log(event.s3);
 
     // TODO implement
     const response = {
