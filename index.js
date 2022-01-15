2c155e17f70697442aef51e12628087f48ec08a2
Rami <44623786+Rami-J@users.noreply.github.com>
2022-01-15 14:27:52 -0500

    Create handleRecord() function

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
 require('dotenv').config();
+const { S3Client, GetObjectCommand } = require('@aws-sdk/client-s3');
+const { fileTypeFromStream } = require('file-type');
+
+const s3Client = new S3Client({ region: process.env.AWS_REGION });
+
+const handleRecord = async (record) => {
+    const bucket = record.s3.bucket.name;
+    const key = record.s3.object.key;
+
+    const getObjectCommand = new GetObjectCommand({
+        Bucket: bucket,
+        Key: key
+    });
+
+    const commandResult = await s3Client.send(getObjectCommand);
+    const fileType = await fileTypeFromStream(commandResult.Body);
+
+    console.log(fileType);
+
+    // TODO: if not valid fileType delete the object
+};
 
 exports.handler = async (event) => {
     console.log(event);
-    console.log(event.userIdentity);
-    console.log(event.requestParameters);
-    console.log(event.responseElements);
-    console.log(event.s3);
 
-    // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
     };
+
+    const records = event.Records;
+
+    try {
+        records.forEach((record) => await handleRecord(record));
+    } catch (error) {
+        console.log(error);
+        response.statusCode = 500;
+    }
+
     return response;
 };
